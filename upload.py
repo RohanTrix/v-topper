ef54a094f3bc3d1cd1380703de6762687f54025e
RohanTrix <rohanyeahyeah@gmail.com>
2021-04-14 12:27:24 +0530

    Commit

--- upload.py
+++ upload.py
@@ -4,7 +4,10 @@ def uploader(driver, class_nbr):
     cmd = "myFunction('"+class_nbr+"');"
     driver.execute_script(cmd)
     time.sleep(1)
-
+    with open('temp.html', 'w+') as f:
+        f.write(driver.page_source)
+    df = pd.read_html('temp.html')
+    print(df[1].iloc[2:,0:7])
     
 
     
